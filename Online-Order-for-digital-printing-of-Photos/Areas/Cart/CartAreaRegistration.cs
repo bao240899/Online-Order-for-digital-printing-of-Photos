f7e3f3ec8994c886471f712073166f441f9a0c7f
Lam <55865874+Lam601@users.noreply.github.com>
2021-03-31 12:20:59 +0700

    add more front-end

---
+++ Online-Order-for-digital-printing-of-Photos/Areas/Cart/CartAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Online_Order_for_digital_printing_of_Photos.Areas.Cart
+{
+    public class CartAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Cart";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Cart_default",
+                "Cart/{Area}/{controller}/{action}/{id}",
+                new { Area="Cart", Controller ="Cart", action = "Cart", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Online-Order-for-digital-printing-of-Photos/Areas/Image/Controllers/DetailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Order_for_digital_printing_of_Photos.Areas.Image.Controllers
+{
+    public class DetailController : Controller
+    {
+        // GET: Image/Detail
+        public ActionResult DetailImg()
+        {
+            return View();
+        }
+    }
+}
