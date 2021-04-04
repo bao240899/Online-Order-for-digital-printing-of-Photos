854a5961d9c7785df4da5694a7cc834aaa111a9b
Lam <55865874+Lam601@users.noreply.github.com>
2021-04-04 14:08:23 +0700

    function Login & add prj Model(databse)

---
+++ Online-Order-for-digital-printing-of-Photos/Common/CommonConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Online_Order_for_digital_printing_of_Photos.Common
+{
+    public static class CommonConstant
+    {
+        public static string USER_SESSION = "USER_SESSION";
+    }
+}
